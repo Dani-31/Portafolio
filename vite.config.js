c580dd35055654ded4b42ef03cb1d7fa9fd69ecd
Dani-31 <rodriguezdani559@gmail.com>
2024-03-15 22:30:53 -0600

    Mi Portafolio

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "https://Dani-31.gitlab.io/Portafolio",
+    base: "/portafolio/",
     appType: 'mpa',
     build: {
         rollupOptions: {
